dfc1ad7e34bcc192bde50f267fb663384066c47f
SnowflakeV <122881251+SnowflakeV@users.noreply.github.com>
2023-01-17 13:26:05 +0100

    Update streamlit_app.py
    Changes in the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Mom\'s New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and & Rockect Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.header('Breakfast Favourites')
+  
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and & Rockect Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
